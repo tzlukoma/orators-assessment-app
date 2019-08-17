a716bf4b004410834a3b504dd7fe71f986728254
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-17 13:32:55 -0400

    Issue #5 Use 'assessments' consistently - Change all referencess to evaluation(s) - Change all references to feedback - Update NewFeedback component to NewAssessment - Update mock data to include assessment resource instead of evaluations

--- src/Components/assessments/NewAssessment.js
+++ src/Components/assessments/NewAssessment.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom'
 import moment from 'moment'
 import FormFields from '../../ui/forms/FormFields'
-import Table from '@material-ui/core/Table'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
-import TableBody from '@material-ui/core/TableBody'
-import FormControl from '@material-ui/core/FormControl'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Radio from '@material-ui/core/Radio'
 import FormLabel from '@material-ui/core/FormLabel'
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
@@ -21,7 +11,7 @@ import StarRatingComponent from 'react-star-rating-component';
 import uuidv4 from 'uuid'
 
 
-class NewFeedback extends Component {
+class NewAssessment extends Component {
     
     constructor(props) {
         super(props);
@@ -134,7 +124,7 @@ class NewFeedback extends Component {
                     <Paper style={{padding:20, marginTop:20}} >
                         <Typography variant="h5" component="h3">
                         <span>{`${this.state.firstname} ${this.state.lastname}`} </span>
-                        <span style={{fontSize:15, fontStyle:'italic'}}>- New Feedback</span>
+                        <span style={{fontSize:15, fontStyle:'italic'}}>- New Assessment</span>
                         </Typography>
                         <Typography>
                             {moment(Date.now()).format("LLL")} 
@@ -215,35 +205,11 @@ class NewFeedback extends Component {
                         </FormGroup>
                         <Container style={{textAlign:'left', marginTop:10}}>
                             <FormLabel style={{fontSize:18, height:14}}>Select a comment:</FormLabel>
-                            
-                            {/* <FormControl component="fieldset" >
-                                <RadioGroup
-                                aria-label="gender"
-                                name="gender1"
-                                value={comment}
-                                onChange={()=>{console.log(comment)}}
-                                >
-                                    <FormControlLabel value="Excellent engagement, effort and participation" control={<Radio />} label="Excellent engagement, effort and participation" />
-                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                                </RadioGroup>
-                            </FormControl>
-                            
-                            <ul>
-                                <li>Excellent engagement, effort and participation</li>
-                                <li>Be sure to incorporate feedback you have been given.</li>
-                                <li>Engage more with the lesson so that you can improve</li>
-                                <li>Well done!</li>
-                                <li>Nice improvement!</li>
-                                <li>Be sure to practice more.  You will improve.</li>
-                                <li>I see that you have been practicing.  Well done!</li>
-                                <li>More focus and effort will give you better results.</li>
-                            </ul> */}
                             <FormFields 
                                 formData={this.state.formData}
                                 change={(newState) => this.updateForm(newState)}
                             />
-                            <Button type="submit" variant="contained" color="primary"style={{textAlign: 'center', marginTop: 30}}>Submit Feedback</Button>
+                            <Button type="submit" variant="contained" color="primary"style={{textAlign: 'center', marginTop: 30}}>Submit Assessment</Button>
                         </Container>
                         
                     </form>
@@ -256,4 +222,4 @@ class NewFeedback extends Component {
     }
 }
 
-export default NewFeedback;
+export default NewAssessment;
