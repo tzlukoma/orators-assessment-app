0c2569dfa578e08cda171745ba134fa32b8a3d08
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-28 17:21:11 -0400

    Issue #51 Add form validation for ADD_ORATOR - Implements redux form with validation

--- src/components/orators/AddOrator.js
+++ src/components/orators/AddOrator.js
@@ -1,36 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { addOrator } from '../../store/actions/oratorActions'
-import { Redirect } from 'react-router-dom'
+import { compose } from 'redux'
 
-import * as ROUTES from '../../constants/routes'
+import { Field, reduxForm } from 'redux-form'
+
 
 class AddOrator extends Component {
-    state = {
-        firstName: '',
-        lastName: '',
-        dateOfBirth:'',
-        chapter_id: 1,
-        family_id: 3
+
+    onSubmit = (e, parent_id, chapter_id, chapter) => {
+        e.parent_id = parent_id;
+        e.chapter_id = chapter_id;
+        e.chapter = chapter
+        console.log(e)
+        // this.props.addOrator(e)
     }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]:e.target.value
-        })
-        
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        // console.log(this.state)
-        this.props.addOrator(this.state)
-    }
-    render() {
-        const { auth } = this.props
-        if(!auth.uid) return <Redirect to={ROUTES.SIGN_IN}/>
+
+    renderInputField(field) {
+        const className = `form-input ${field.meta.touched && field.meta.error ? 'pink lighten-5' : ''}`
+
         return (
-            
+            <div className={className}>
+                <label htmlFor={field.name}>{field.myLabel}</label>
+                <input type={field.type} {...field.input}></input>
+                <div className="pink-text center">
+                    {field.meta.touched ? field.meta.error : ''}
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        const { auth, profile, authError } = this.props
+        // console.log(auth)
+        // if (!auth.uid) return <Redirect to={ROUTES.SIGN_IN} />
+        return (
+
             <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">
+
+                <form onSubmit={this.props.handleSubmit((event) => this.onSubmit(event, auth.uid, profile.chapter_id,profile.chapter))}>
+
+                    <Field
+                        myLabel="First Name"
+                        placeholder="Enter your first name"
+                        type="text"
+                        name="firstName"
+                        component={this.renderInputField}
+                    />
+                    <Field
+                        myLabel="Last Name"
+                        name="lastName"
+                        type="text"
+                        component={this.renderInputField}
+                    />
+                                        <Field
+                        myLabel="Birth Date"
+                        name="dateOfBirth"
+                        type="date"
+                        component={this.renderInputField}
+                    />
+                    <div className="input-field">
+                        <div className="red-text center">
+                            {authError ? <p className="pink-text">{authError}</p> : null}
+                        </div>
+                        <button className="btn deep-purple lighten-1 z-depth-0">Add Orator</button>
+                    </div>
+                </form>
+
+                {/* <form onSubmit={this.handleSubmit} className="white">
                     <h5 className="grey-text text-darken-3">Add Orator</h5>
                     <div className="input-field">
                         <label htmlFor="firstName">First Name</label>
@@ -47,16 +84,18 @@ class AddOrator extends Component {
                     <div className="input-field">
                         <button className="btn deep-purple lighten-1 z-depth-0">Register Orator</button>
                     </div>
-                </form>
+                </form> */}
             </div>
-            
+
         );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError,
+        profile: state.firebase.profile
     }
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
@@ -67,5 +106,29 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddOrator);
+function validate(values) {
+    const errors = {};
+
+    if (!values.firstName) {
+        errors.firstName = "The first name is empty"
+    }
+    if (!values.lastName) {
+        errors.lastName = "The last name is empty"
+    }
+    if(!values.dateOfBirth){
+        errors.dateOfBirth = "The birth date is empty"
+    } else if(!/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i.test(values.dateOfBirth)){
+        errors.dateOfBirth = "This date is not valid"
+    }
+
+    return errors;
+}
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    reduxForm({
+        validate,
+        form: 'addOratorDetails'
+    })
+)(AddOrator)
 
