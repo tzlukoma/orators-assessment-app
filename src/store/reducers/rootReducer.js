694d7ee4a9bc5a959d09f54a1ebfdcecd4676a72
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-28 01:13:16 -0400

    Issue #18 - Initial redux form set up - validation for correct chapter code working on sign-up form

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,13 +4,15 @@ import oratorReducer from './oratorReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
 import { firebaseReducer } from 'react-redux-firebase'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     assessment: assessmentReducer,
     orator: oratorReducer,
     firestore: firestoreReducer,
-    firebase: firebaseReducer
+    firebase: firebaseReducer,
+    form: formReducer
 })
 
 export default rootReducer
