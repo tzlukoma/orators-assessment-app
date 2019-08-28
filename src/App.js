2db3af0c04c7b10c08bf961aaa88d54f286c3182
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-27 20:51:19 -0400

    #13 Add component for non-existent routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 import CoachSignUp from './components/auth/CoachSignUp'
 import CoachWelcome from './components/onboarding/CoachWelcome'
+import NotFound from './components/auth/NotFound'
+
 import * as ROUTES from './constants/routes';
 
 function App() {
@@ -18,30 +20,16 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route
-            exact
-            path={ROUTES.DASHBOARD}
-            component={Dashboard} />
-          <Route
-            path={`${ROUTES.ASSESSMENT}/:id`}
-            component={AssessmentDetails}
-          />
-          <Route
-            path={`${ROUTES.ORATOR}/:id`}
-            component={OratorDetails} />
-          <Route
-            path={`${ROUTES.CREATE_ASSESSMENT}/:id/:firstname/:lastname`}
-            component={CreateAssessment}
-          />
-          <Route
-            path={`${ROUTES.ADD_ORATOR}/:chapter_id/:family_id`}
-            component={AddOrator}
-          />
+          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route path={`${ROUTES.ASSESSMENT}/:id`} component={AssessmentDetails} />
+          <Route path={`${ROUTES.ORATOR}/:id`} component={OratorDetails} />
+          <Route path={`${ROUTES.CREATE_ASSESSMENT}/:id/:firstname/:lastname`} component={CreateAssessment} />
+          <Route path={`${ROUTES.ADD_ORATOR}/:chapter_id/:family_id`} component={AddOrator} />
           <Route path={ROUTES.SIGN_IN} component={SignIn} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
           <Route path={ROUTES.COACH_SIGNUP} component={CoachSignUp} />
           <Route path={ROUTES.COACH_WELCOME} component={CoachWelcome} />
-          {/* <Route component={NotFound} /> */}
+          <Route component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/auth/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="pink-text container center" style={{ margin: 'auto', padding:20 }}>
+            <h4>Los siento!! You have reached a page that does not exist</h4>
+        </div>
+    )
+}
+
+export default (NotFound)
