2a4593d32e328c15eeca79e7ec7d3ed1e18d8b32
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-30 20:35:24 -0400

    Issue #58 OratorSummary tests and refactoring

--- src/components/orators/OratorSummary.js
+++ src/components/orators/OratorSummary.js
@@ -2,12 +2,15 @@ import React from 'react';
 import moment from 'moment'
 
 const OratorSummary = (props) => {
+    
+    const { firstName, lastName, dateOfBirth, parentName} = props.orator
+
     return (
         <div className="card z-depth-1 orator-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">{props.orator.firstName} {props.orator.lastName}</span>
-                <p>{moment().diff(props.orator.dateOfBirth, 'years',false)} years old</p>
-                <p className="grey-text">{props.orator.family} Family</p>
+                <span className="card-title">{firstName} {lastName}</span>
+                <p>{moment().diff(dateOfBirth, 'years',false)} years old</p>
+                <p className="grey-text">{parentName}'s Family</p>
             </div>
         </div>
     );
