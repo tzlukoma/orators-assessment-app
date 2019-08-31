b3efa72aead52afa797a5506610df54775df1c76
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-08-31 18:07:56 -0400

    Add conditional render for Coach vs. Family view

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,73 +1,29 @@
 import React, { Component } from 'react';
-import AssessmentList from '../assessments/AssessmentList'
-import OratorList from '../orators/OratorList'
-import Notifications from './Notifications'
 import { connect } from 'react-redux'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
+import CoachView from './CoachView';
+import FamilyView from './FamilyView'
 
 class Dashboard extends Component {
-    
-    renderCoachView = (notifications, orators) => {
-        return(
-            <div>
-            <div className="col s12 m5 l4 offset-m1">
-                        <h4>Notifications</h4>
-                        <Notifications notifications={notifications}/>
-                    </div>
-                    <div className="col s12 m5 l4 offset-m1">
-                        <h4>Orators</h4>
-                        <OratorList orators={orators}/>
-                    </div>
-            </div>
-        )
-    }
-
-    renderParentView = (assessments, orators) => {
-        return(<div>
-            <div className="col s12 m5 l4 offset-m1">
-                        <h4>Assessments</h4>
-                        <AssessmentList assessments={assessments}/>
-                    </div>
-                    <div className="col s12 m5 l4 offset-m1">
-                        <h4>Orators</h4>
-                        <OratorList orators={orators}/>
-                    </div>
-            </div>
-        )
-    }
-
     render() {
-        const { assessments, orators, notifications, profile } = this.props
+        const { profile } = this.props
         
         return (
             <div className="dashboard container">
-                <div className="row">
                     {profile.coach ? 
-                        this.renderCoachView(notifications, orators)
+                         <CoachView />
                         : 
-                        this.renderParentView(assessments, orators)
+                        <FamilyView />
                     }
                 </div>
-            </div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        assessments: state.firestore.ordered.assessments,
-        orators: state.firestore.ordered.orators,
         profile: state.firebase.profile,
-        notifications: state.firestore.ordered.notifications
     }
 }
 
-export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([
-        { collection: 'assessments', limit: 2, orderBy: ['createdAt','desc'] },
-        { collection: 'orators', limit: 2, orderBy: ['dateOfBirth', 'asc']},
-        { collection: 'notifications', limit:2, orderBy: ['time', 'desc']}
-    ])
-)(Dashboard)
+export default connect(mapStateToProps)(Dashboard)
+
