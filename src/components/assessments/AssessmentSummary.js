4107145d54029bffd4e19e3ca159d769dd172cea
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-09-02 18:30:22 -0400

    Issue #6 Adds calcuation of rating average

--- src/components/assessments/AssessmentSummary.js
+++ src/components/assessments/AssessmentSummary.js
@@ -8,7 +8,8 @@ const AssessmentSummary = (props) => {
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title"> Assessment for {firstName} {lastName}</span>
                 <h6 style={{marginBottom:10}}>{comment} </h6>
-                <div className="card-action grey lighten-4 grey-text">
+                <h6 className="deep-purple-text"style={{padding: '5px 0'}}>Average Rating: {props.avg.toFixed(2)}</h6>
+                <div className="card-action grey lighten-4 grey-text" style={{padding: '5px 10px'}}>
                     <div>Posted by {coachFirstName} {coachLastName}</div>
                     <div>{moment(createdAt.toDate()).fromNow()}</div>
                 </div>
