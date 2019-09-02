3b75ae82fd8d12e7a4fa4a3e878ec3b7003c9ae5
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-09-02 15:58:44 -0400

    Add sign in and sign up buttons to home page

--- src/components/info/Home.js
+++ src/components/info/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes'
 
 const Home = ({ user }) => {
-    if(user) return <Redirect to={ROUTES.DASHBOARD} />
+    if (user) return <Redirect to={ROUTES.DASHBOARD} />
     return (
-        <div className="container valign-wrapper" style={{height:'30rem', padding:50}}>
+        <div className="container" style={{ height: '30rem', padding: 30 }}>
+            <div className="container" style={{ padding: 10 }}>
             <h4 className="center">This is the home page where we describe everything about the app.</h4>
+            </div>
+            <div className="container center" style={{padding: 10}}>
+                <Link to={ROUTES.SIGN_IN} className="btn blue">Login</Link><Link to={ROUTES.SIGN_UP} className="btn blue lighten-2" style={{marginLeft:10}}>Sign-up</Link>
+            </div>
         </div>
     );
 };
