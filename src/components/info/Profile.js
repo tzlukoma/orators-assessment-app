311529c9a7440a00cd35c65f24850c6cde353106
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-09-05 22:57:14 -0400

    Adds initial profile page

--- src/components/info/Profile.js
+++ src/components/info/Profile.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Link, Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+import { signOut } from '../../store/actions/authActions'
 
 import * as ROUTES from '../../constants/routes'
 
-const Profile = ({ auth, profile }) => {
+const Profile = ({signOut, profile, auth}) => {
     if (!auth.uid) return <Redirect to={ROUTES.SIGN_IN}/>
     else { 
         return (
         
             <div >
                 <div className="container center">
-                    <h4 className="deep-purple-text" style={{padding: 20}}>Welcome to<br>
-                    </br>The Orators App</h4>
-                    <Link to={ROUTES.DASHBOARD} className="btn white-text blue lighten-3 center z-depth-0">Go to the Dashboard</Link>
+                    <h4 className="deep-purple-text" style={{padding: 20}}>
+                    {profile.firstName} {profile.lastName}</h4>
+                    <btn onClick={signOut} className="btn white-text blue lighten-3 center z-depth-0">Sign Out</btn>
                 </div>
             </div>
     
@@ -22,7 +23,8 @@ const Profile = ({ auth, profile }) => {
     
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+    console.log(state)
     return {
         auth: state.firebase.auth,
         profile: state.firebase.profile
@@ -30,5 +32,10 @@ const mapStateToProps = (state) => {
 }
 
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
 
-export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
