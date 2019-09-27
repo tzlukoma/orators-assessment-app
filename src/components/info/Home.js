1a8d0b9d894d2c82ea4d23c272b2df55f1aeaa71
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-09-26 20:45:13 -0400

    Adds NJ Orators logo on home page

--- src/components/info/Home.js
+++ src/components/info/Home.js
@@ -9,7 +9,11 @@ const Home = ({ user }) => {
         <div className="home-container" style={{ padding: 30 }}>
             <div className="container" style={{ padding: 10 }}>
             <h4 className="center white-text">Welcome to the Orators App.</h4>
+            <div className="kids-illustration" style={{margin:'1rem', align:'center', padding:'1rem'}}>
             <img style={{margin: '20px 10px'}}alt="illustration of young people"src="/img/orators-in-action.png"></img>
+
+            </div>
+
             </div>
             <div className="container center" style={{padding: 10}}>
                 <Link to={ROUTES.SIGN_IN} className="btn blue z-depth-0">Login</Link>
@@ -18,7 +22,10 @@ const Home = ({ user }) => {
             <div className="container white-text center" >
             <Link to={ROUTES.COACH_SIGNUP} style={{textDecoration:'none'}}>Coach Sign Up</Link>
             </div>
-            
+            <div className="orators-logo"  style={{margin:'10px', align:'center'}}>
+            <img alt="orators-logo"src="img/nj.orators-logo.png"></img>
+
+            </div>
         </div>
     );
 };
