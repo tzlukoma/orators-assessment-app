e9c9e4359244bd9b368b828417ebef46d4702daa
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2019-10-28 22:39:27 -0400

    Issue #113 File upload - Updates orator data based on file from oct-2802019 - Adds Union chapter and chapter code

--- src/_ref/chapters.js
+++ src/_ref/chapters.js
@@ -1,112 +1,117 @@
 export default [
-    {
-      "id": "NJ-FREHLD",
-      "label": "Freehold, NJ",
-      "value": "Freehold, NJ"
-    },
-    {
-      "id": "NJ-LAKEWD",
-      "label": "Lakewood, NJ",
-      "value": "Lakewood, NJ"
-    },
-    {
-      "id": "NJ-MRSCTY",
-      "label": "Morris County, NJ",
-      "value": "Morris County, NJ"
-    },
-    {
-      "id": "NJ-NBRUNS",
-      "label": "New Brunswick, NJ",
-      "value": "New Brunswick, NJ"
-    },
-    {
-      "id": "NJ-NEPTUN",
-      "label": "Neptune, NJ",
-      "value": "Neptune, NJ"
-    },
-    {
-      "id": "NJ-NEWRK1",
-      "label": "Newark I, NJ",
-      "value": "Newark I, NJ"
-    },
-    {
-      "id": "NJ-NEWRK2",
-      "label": "Newark II, NJ",
-      "value": "Newark II, NJ"
-    },
-    {
-      "id": "NJ-NUTHILL",
-      "label": "Nutley/Hillside, NJ",
-      "value": "Nutley/Hillside, NJ"
-    },
-    {
-      "id": "NJ-ORANGE",
-      "label": "Orange, NJ",
-      "value": "Orange, NJ"
-    },
-    {
-      "id": "NJ-PERAMB",
-      "label": "Perth Amboy, NJ",
-      "value": "Perth Amboy, NJ"
-    },
-    {
-      "id": "NJ-PISCAT1",
-      "label": "Piscataway I, NJ",
-      "value": "Piscataway I, NJ"
-    },
-    {
-      "id": "NJ-PISCAT2",
-      "label": "Piscataway II, NJ",
-      "value": "Piscataway II, NJ"
-    },
-    {
-      "id": "NJ-PLNFLD",
-      "label": "Plainfield, NJ",
-      "value": "Plainfield, NJ"
-    },
-    {
-      "id": "NJ-REDBNK",
-      "label": "Red Bank, NJ",
-      "value": "Red Bank, NJ"
-    },
-    {
-      "id": "NJ-SCTPLN",
-      "label": "Scotch Plains, NJ",
-      "value": "Scotch Plains, NJ"
-    },
-    {
-      "id": "NJ-SOMSET",
-      "label": "Somerset, NJ",
-      "value": "Somerset, NJ"
-    },
-    {
-      "id": "NJ-SOUMPL",
-      "label": "South Orange/Maplewood, NJ",
-      "value": "South Orange/Maplewood, NJ"
-    },
-    {
-      "id": "NJ-TRENTN",
-      "label": "Trenton, NJ",
-      "value": "Trenton, NJ"
-    },
-    {
-      "id": "NJ-WILBUR",
-      "label": "Willingboro/Burlington, NJ",
-      "value": "Willingboro/Burlington, NJ"
-    },
-    {
-      "id": "NJ-WSWPLN",
-      "label": "West Windsor/Plainsboro",
-      "value": "West Windsor/Plainsboro"
-    },
-    {
-      "id": "PA-EASTON",
-      "label": "Easton, PA",
-      "value": "Easton, PA"
-    },
-    {
-      "id": "PA-STELTN",
-      "label": "Steelton, PA",
-      "value": "Steelton, PA"
-    }
-  ]
+  {
+    "id": "NJ-FREHLD",
+    "label": "Freehold, NJ",
+    "value": "Freehold, NJ"
+  },
+  {
+    "id": "NJ-LAKEWD",
+    "label": "Lakewood, NJ",
+    "value": "Lakewood, NJ"
+  },
+  {
+    "id": "NJ-MRSCTY",
+    "label": "Morris County, NJ",
+    "value": "Morris County, NJ"
+  },
+  {
+    "id": "NJ-NBRUNS",
+    "label": "New Brunswick, NJ",
+    "value": "New Brunswick, NJ"
+  },
+  {
+    "id": "NJ-NEPTUN",
+    "label": "Neptune, NJ",
+    "value": "Neptune, NJ"
+  },
+  {
+    "id": "NJ-NEWRK1",
+    "label": "Newark I, NJ",
+    "value": "Newark I, NJ"
+  },
+  {
+    "id": "NJ-NEWRK2",
+    "label": "Newark II, NJ",
+    "value": "Newark II, NJ"
+  },
+  {
+    "id": "NJ-NUTHILL",
+    "label": "Nutley/Hillside, NJ",
+    "value": "Nutley/Hillside, NJ"
+  },
+  {
+    "id": "NJ-ORANGE",
+    "label": "Orange, NJ",
+    "value": "Orange, NJ"
+  },
+  {
+    "id": "NJ-PERAMB",
+    "label": "Perth Amboy, NJ",
+    "value": "Perth Amboy, NJ"
+  },
+  {
+    "id": "NJ-PISCAT1",
+    "label": "Piscataway I, NJ",
+    "value": "Piscataway I, NJ"
+  },
+  {
+    "id": "NJ-PISCAT2",
+    "label": "Piscataway II, NJ",
+    "value": "Piscataway II, NJ"
+  },
+  {
+    "id": "NJ-PLNFLD",
+    "label": "Plainfield, NJ",
+    "value": "Plainfield, NJ"
+  },
+  {
+    "id": "NJ-REDBNK",
+    "label": "Red Bank, NJ",
+    "value": "Red Bank, NJ"
+  },
+  {
+    "id": "NJ-SCTPLN",
+    "label": "Scotch Plains, NJ",
+    "value": "Scotch Plains, NJ"
+  },
+  {
+    "id": "NJ-SOMSET",
+    "label": "Somerset, NJ",
+    "value": "Somerset, NJ"
+  },
+  {
+    "id": "NJ-SOUMPL",
+    "label": "South Orange/Maplewood, NJ",
+    "value": "South Orange/Maplewood, NJ"
+  },
+  {
+    "id": "NJ-TRENTN",
+    "label": "Trenton, NJ",
+    "value": "Trenton, NJ"
+  },
+  {
+    "id": "NJ-UNIONX",
+    "label": "Union, NJ",
+    "value": "Union, NJ"
+  },
+  {
+    "id": "NJ-WILBUR",
+    "label": "Willingboro/Burlington, NJ",
+    "value": "Willingboro/Burlington, NJ"
+  },
+  {
+    "id": "NJ-WSWPLN",
+    "label": "West Windsor/Plainsboro",
+    "value": "West Windsor/Plainsboro"
+  },
+  {
+    "id": "PA-EASTON",
+    "label": "Easton, PA",
+    "value": "Easton, PA"
+  },
+  {
+    "id": "PA-STELTN",
+    "label": "Steelton, PA",
+    "value": "Steelton, PA"
+  }
+]
